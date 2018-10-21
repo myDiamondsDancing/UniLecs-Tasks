ecf4c0bd3bc761f75e4c75432d9e04927627f97f
Dmitriy <38916727+myDiamondsDancing@users.noreply.github.com>
2018-10-21 10:57:46 +0300

    Update Task 133.cs

--- Task 133.cs
+++ Task 133.cs
@@ -21,6 +21,6 @@ public class Program
     {
         Console.WriteLine("GetCountOfPoints(10, 8, 4, 7) : {0}", GetCountOfPoints(10, 8, 4, 7));
         Console.WriteLine("GetCountOfPoints(10, 8, 7, 4) : {0}", GetCountOfPoints(10, 8, 7, 4));
-	Console.WriteLine("GetCountOfPoints(3, 3, 2, 1) : {0}", GetCountOfPoints(3, 3, 2, 1)); 
+        Console.WriteLine("GetCountOfPoints(3, 3, 2, 1) : {0}", GetCountOfPoints(3, 3, 2, 1)); 
     }
 }
