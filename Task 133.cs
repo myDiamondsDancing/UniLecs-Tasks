36a5a8fb1152f029db0571263058ed1824effef2
Dmitriy <38916727+myDiamondsDancing@users.noreply.github.com>
2018-10-21 10:59:05 +0300

    Update Task 133.cs

--- Task 133.cs
+++ Task 133.cs
@@ -17,8 +17,8 @@ public class Program
         return count;
     }
 
-        public static void Main()
-        {
+   public static void Main()
+   {
         Console.WriteLine("GetCountOfPoints(10, 8, 4, 7) : {0}", GetCountOfPoints(10, 8, 4, 7));
         Console.WriteLine("GetCountOfPoints(10, 8, 7, 4) : {0}", GetCountOfPoints(10, 8, 7, 4));
         Console.WriteLine("GetCountOfPoints(3, 3, 2, 1) : {0}", GetCountOfPoints(3, 3, 2, 1)); 
