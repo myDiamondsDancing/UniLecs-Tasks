9f20c8724ba0e6d24fbb52f283d7aae3292777c6
Dmitriy <38916727+myDiamondsDancing@users.noreply.github.com>
2018-10-21 10:45:20 +0300

    Update Task 133.cs

--- Task 133.cs
+++ Task 133.cs
@@ -5,22 +5,22 @@ public class Program
 
     public static int GetCountOfPoints(int N, int M, int A, int B)
     {
-        int count = N * (A - 1);
-        if A % 2 = 1
-        {
-            count += B;
-        }
-        else
-        {
-            count += M - B + 1;
-        }
-        return count;
-
+		int count = N * (A - 1);
+		if (A % 2 == 1)
+		{
+			count += B;
+		}
+		else
+		{
+			count += N - B + 1;
+		}
+		return count;
     }
 
     public static void Main()
     {
-        Console.WriteLine(string.format('Data : {0}, answer = {1}', GetCountOfPoints(10, 8, 4, 7));
-        Console.WriteLine(string.format('Data : {0}, answer = {1}', GetCountOfPoints(10, 8, 7, 4));   
+        Console.WriteLine("GetCountOfPoints(10, 8, 4, 7) : {0}", GetCountOfPoints(10, 8, 4, 7));
+        Console.WriteLine("GetCountOfPoints(10, 8, 7, 4) : {0}", GetCountOfPoints(10, 8, 7, 4));
+		Console.WriteLine("GetCountOfPoints(3, 3, 2, 1) : {0}", GetCountOfPoints(3, 3, 2, 1)); 
     }
 }
