a8b5080ea4d817dc5d2304492eca98ba2047b19c
Dmitriy <38916727+myDiamondsDancing@users.noreply.github.com>
2018-10-28 19:02:33 +0200

    Update Task 135.py

--- Task 135.py
+++ Task 135.py
@@ -2,18 +2,19 @@ from math import ceil
 
 def main(N):
     if N == 0:
-        return 0
+        return 1
     elif N == 1:
         return 1
     elif N == 2:
         return 1
     elif N == 3: 
         return 2
+    elif N == 4:
+        return 2    
     else:
-        count = 3
-        for i in range(ceil(N / 2), N - 2):
+        count = 0
+        for i in range(ceil(N / 2), N + 1):
             count += main(N - i) 
         return count 
 
-for i in (5, 7, 9, 11, 100):
-    print(main(i))  
+print(main(11))
