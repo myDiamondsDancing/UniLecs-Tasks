be3690c176762f3d96b75c40ed4971275d447714
Dmitriy <38916727+myDiamondsDancing@users.noreply.github.com>
2018-10-28 21:17:36 +0200

    Update Task 135.py

--- Task 135.py
+++ Task 135.py
@@ -1,20 +1,32 @@
-from math import ceil
+def get_summands(n):
+    # Благодарю @jinxonik за предоставленный фрагмент кода из 130-ой задачи
+    def generate(n, max_i, cur_list):
+        max_i = min(n, max_i)
+        yield cur_list + [1] * n
 
-def main(N):
-    if N == 0:
-        return 1
-    elif N == 1:
-        return 1
-    elif N == 2:
-        return 1
-    elif N == 3: 
-        return 2
-    elif N == 4:
-        return 2    
-    else:
-        count = 0
-        for i in range(ceil(N / 2), N + 1):
-            count += main(N - i) 
-        return count 
+        for i in range(2, max_i + 1):
+            if n - i == 0:
+                yield cur_list + [i]
+            else:
+                yield from generate(n - i, i, cur_list + [i])
 
-print(main(11))
+    if n <= 0: return None
+    yield from generate(n, n, [])
+    
+def flag(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] <= arr[i + 1]:
+            return False
+    return True    
+        
+def main(n):
+    listOfSummands = list(get_summands(n))  
+    count = len(listOfSummands)
+    for el in listOfSummands:
+        if not flag(el):
+            count -= 1
+                        
+    return count
+
+for i in (3, 6, 8, 10, 15):
+    print('{0} : {1}'.format(i, main(i))) 
