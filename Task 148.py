cee83e67a5a11d59f38313256290e85e1aebc392
Dmitriy <38916727+myDiamondsDancing@users.noreply.github.com>
2018-12-24 09:59:46 +0200

    Update Task 148.py

--- Task 148.py
+++ Task 148.py
@@ -2,14 +2,16 @@ def main(arr : list) -> list:
      #Идея проста: создаем массив такой же длины, что и массив на входе
      #А затем просто в начало добавляем ненулевые элементы
      
-     arr2 = [0] * len(arr)
      i = 0
      
      for j in range(len(arr)):
         if arr[j] != 0 :
-            arr2[i] = arr[j]
+            arr[i] = arr[j]
             i += 1
+            
+     for k in range(i, len(arr)):
+        arr[k] = 0     
 
-     return arr2
+     return arr
 
 print(main([0, 4, 5, -1, 9, 9, 0, 54]))    
